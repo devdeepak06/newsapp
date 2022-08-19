a42c0178ca60f83dd0dbbc77b46804d792838bd1
Deepak Kumar <104432396+devdeepak06@users.noreply.github.com>
2022-08-19 21:16:45 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+
+import React, { Component } from 'react';
+import NavBar from './components/NavBar';
+import News from './components/News';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+} from "react-router-dom";
+
+export default class App extends Component {
+    pageSize = 15;
+    country = "in";
+    render() {
+        return (
+            <div>
+                <Router>
+                    <NavBar />
+                    <Switch>
+                        <Route exact path="/"><News key="general" pageSize={this.pageSize} country={this.country} category="general" /></Route>
+                        <Route exact path="/business"><News key="business" pageSize={this.pageSize} country={this.country} category="business" /></Route>
+                        <Route exact path="/entertainment"><News key="entertainment" pageSize={this.pageSize} country={this.country} category="entertainment" /></Route>
+                        <Route exact path="/health"><News key="health" pageSize={this.pageSize} country={this.country} category="health" /></Route>
+                        <Route exact path="/science"><News key="science" pageSize={this.pageSize} country={this.country} category="science" /></Route>
+                        <Route exact path="/sports"><News key="sports" pageSize={this.pageSize} country={this.country} category="sports" /></Route>
+                        <Route exact path="/technology"><News key="technology" pageSize={this.pageSize} country={this.country} category="technology" /></Route>
+                    </Switch>
+                </Router>
+            </div>
+        )
+    }
+}
+
